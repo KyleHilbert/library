2c5772cd08339838bea1869b450fbb63bc1a6145
Kyle Hilbert <104146726+KyleHilbert@users.noreply.github.com>
2022-11-01 09:58:40 -0400

    :/

---
+++ javascript.js
@@ -0,0 +1,24 @@
+/* Objective: 
+    When plus button is hit bring up a form to add a new book with
+    the details of the book
+    Form needs: 
+      - Author
+      - Title
+      - Number of pages
+      - If you have read it 
+
+
+All of your book objects are going to be stored in a simple array, 
+so add a function to the script (not the constructor) that can take 
+user’s input and store the new book objects into an array. 
+*/
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
